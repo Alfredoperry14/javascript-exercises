a6f4c16791f0eb3f7b73e79e6eddddeb4a40d013
Alfredo Perry <alfredoperry14@gmail.com>
2022-07-25 18:44:06 -0700

    Exercise 7 Solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp -= 32;
+  let double = 5/9;
+  return parseFloat((temp * double).toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let double = 9/5
+  return parseFloat((temp * double + 32).toFixed(1));
 
 };
 
