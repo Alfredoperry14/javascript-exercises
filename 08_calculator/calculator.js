4223226de483ed8a31a39858619b4e3ed592a250
Alfredo Perry <alfredoperry14@gmail.com>
2022-07-25 19:11:56 -0700

    Solution to exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...args) {
+  let total = 0;
+    for(let i = 0; i < args.length; i++){
+      total += args[i];
+    }
+    return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  return args[0] - args[1];  
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let total = 0;
+	for(let i = 0; i < args[0].length; i++){
+    total += args[0][i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let total = 0 + args[0][0];
+  for(let i = 1; i < args[0].length; i++){
+    total *= args[0][i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+    let total = num;
+  for(let i = 1; i < power; i++){
+      total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ans = 1;
+  for(let i = 1; i <= num; i++)
+    ans *= i; 
+  return ans;
 };
 
 // Do not edit below this line
