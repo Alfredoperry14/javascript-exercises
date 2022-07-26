77faf081ee691a86eed30fc188e7499daf57f008
Alfredo Perry <alfredoperry14@gmail.com>
2022-07-25 18:15:46 -0700

    Exercise 4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    let counter = 0, flag = 0;
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < args.length;j++){
+            flag = 0;
+            if(array[i] === args[j]){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0){
+            newArray[counter] = array[i];
+            counter++;
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
